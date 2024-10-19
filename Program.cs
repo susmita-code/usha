3cb9747c3982d871a0d3c3a7174f714ac497b9eb
Student <Student@LAB513-PC20>
2024-10-19 11:07:00 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+public class FirstSelenium
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("https://practicetestautomation.com/practice-test-login/");
+        Console.WriteLine("Open Browser");
+
+        driver.Manage().Window.Maximize();
+        Console.WriteLine("Browser Maximize");
+
+        driver.FindElement(By.Id("username")).SendKeys("students");
+        Console.WriteLine("Provide username");
+
+        driver.FindElement(By.Id("password")).SendKeys("Password123");
+        Console.WriteLine("Provide Password");
+
+        driver.FindElement(By.Id("submit")).Click();
+        Console.WriteLine("Hit Submit button");
+        try
+        {
+            driver.FindElement(By.CssSelector(".wp-block-button__link")).Click();
+        }
+        catch
+        {
+            Console.WriteLine("Failed Login");
+        }
+
+    }
+}
