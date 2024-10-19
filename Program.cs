f3a540e6810aabb623eda84e6a171d9866c6f17c
student <Password123>
2024-10-19 12:43:41 +0600

    #extent report

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
-﻿using OpenQA.Selenium;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+
 public class FirstSelenium
 {
+
     static void Main()
     {
         IWebDriver driver = new ChromeDriver();
+        ExtentReports extentReport = new ExtentReports();
+        ExtentSparkReporter reportpath = new ExtentSparkReporter(@"C:\Report Location"+DateTime .Now+ ".+ html");
+        extentReport.AttachReporter(reportpath);
+        ExtentTest test = extentReport.CreateTest("Login Test", "This is our first test case");
+ 
 
         driver.Navigate().GoToUrl("https://practicetestautomation.com/practice-test-login/");
+
+        test.Log(Status.Info, "Open Browser");
         Console.WriteLine("Open Browser");
 
         driver.Manage().Window.Maximize();
@@ -28,6 +39,11 @@ public class FirstSelenium
         {
             Console.WriteLine("Failed Login");
         }
+        driver.Quit();
+        extentReport.Flush();
+
+
 
     }
 }
+
