77c462e6086f552722b8cc83116b4482874fa3da
malfoysal <foysal8353@gmailcom>
2024-10-26 15:11:06 +0600

    2nd day push

---
+++ Classdata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usha
+{
+    internal class TestData
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,13 +2,19 @@
 using AventStack.ExtentReports.Reporter;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Globalization;
+using usha;
+using CsvHelper;
 
 public class FirstSelenium
 {
+    public static string DataCSVFile = System.IO.Directory.GetCurrentDirectory();
 
 
     static void Main()
     {
+        var testDataList = ReadCsvData(DataCSVFile+ "\\UshaData\\ushaProject-1.csv");
+        
         CreateReportDirectories();
         IWebDriver driver = new ChromeDriver();
         ExtentReports extentReport = new ExtentReports();
@@ -20,35 +26,43 @@ public class FirstSelenium
 
         driver.Navigate().GoToUrl("https://practicetestautomation.com/practice-test-login/");
 
+        foreach(var testaData in testDataList)
+        {
+            driver.FindElement(By.Id("username")).SendKeys(testaData.username);
+
+            test.Log(Status.Info, "Provide username  "+ testaData.username);
+            Console.WriteLine("Provide username " + testaData.username);
+
+            driver.FindElement(By.Id("password")).SendKeys(testaData.password);
+            test.Log(Status.Info, "Provide Password "+ testaData.password);
+            Console.WriteLine("Provide Password "+ testaData.password);
+
+            driver.FindElement(By.Id("submit")).Click();
+            test.Log(Status.Info, "Hit Submit button");
+            Console.WriteLine("Hit Submit button");
+            try
+            {
+
+                driver.FindElement(By.CssSelector(".wp-block-button__link")).Click();
+                test.Log(Status.Info, "LOgin Successfully");
+            }
+            catch
+            {
+                Console.WriteLine("Failed Login");
+                test.Log(Status.Info, "LOgin failed");
+            }
+            
+        }
+
         test.Log(Status.Info, "Open Browser");
         Console.WriteLine("Open Browser");
 
         driver.Manage().Window.Maximize();
         Console.WriteLine("Browser Maximize");
+        
 
-        driver.FindElement(By.Id("username")).SendKeys("students");
-        test.Log(Status.Info, "Provide username");
-        Console.WriteLine("Provide username");
-
-        driver.FindElement(By.Id("password")).SendKeys("Password123");
-        test.Log(Status.Info, "Provide Password");
-        Console.WriteLine("Provide Password");
-
-        driver.FindElement(By.Id("submit")).Click();
-        test.Log(Status.Info, "Hit Submit button");
-        Console.WriteLine("Hit Submit button");
-        try
-        {
-            driver.FindElement(By.CssSelector(".wp-block-button__link")).Click();
-            test.Log(Status.Info, "LOgin Successfully");
-        }
-        catch
-        {
-            Console.WriteLine("Failed Login");
-            test.Log(Status.Info, "LOgin failed");
-        }
-        driver.Quit();
-        extentReport.Flush();
+       
+        
 
 
 
@@ -61,6 +75,16 @@ public class FirstSelenium
         if (Directory.Exists(ReportPath))
         {
             Directory.CreateDirectory(ReportPath);
+        }
+    }
+
+
+    static List<TestData> ReadCsvData(string filePath)
+    {
+        using (var reader = new StreamReader(filePath))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            return new List<TestData>(csv.GetRecords<TestData>());
         }
     }
 }
